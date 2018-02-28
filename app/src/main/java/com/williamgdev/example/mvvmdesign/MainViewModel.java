e3055b4e62894b14a727408bf2f09069276b9bc4
AndroidStorm <williamg.dev@gmail.com>
2018-02-28 11:47:50 -0800

    Added a ImageViewAdapter

--- app/src/main/java/com/williamgdev/example/mvvmdesign/MainViewModel.java
+++ app/src/main/java/com/williamgdev/example/mvvmdesign/MainViewModel.java
@@ -2,13 +2,16 @@ package com.williamgdev.example.mvvmdesign;
 
 
 import android.databinding.BaseObservable;
+import android.databinding.BindingAdapter;
 import android.databinding.ObservableField;
+import android.widget.ImageView;
 
 import com.williamgdev.example.mvvmdesign.databinding.ActivityMainBinding;
 
 public class MainViewModel extends BaseObservable {
     private ActivityMainBinding binding;
-    public static ObservableField<String> message = new ObservableField<>();
+    public static ObservableField<String> message = new ObservableField<>("Do something");
+    public static ObservableField<String> url = new ObservableField<>("OK");
 
     public MainViewModel(ActivityMainBinding binding) {
         this.binding = binding;
@@ -18,6 +21,14 @@ public class MainViewModel extends BaseObservable {
     private void init() {
         binding.button.setOnClickListener(v -> {
             message.set("hello");
+            url.set("SHOW");
         });
+    }
+
+    @BindingAdapter("ImageAdapter")
+    public static void changeImage(ImageView imageView, ObservableField<String> url) {
+        if(!url.get().equalsIgnoreCase("OK")) {
+            imageView.setImageResource(R.mipmap.ic_launcher_round);
+        }
     }
 }
